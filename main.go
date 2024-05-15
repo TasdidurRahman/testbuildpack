dd3f415d8cb1b273e483e338ce3d7b828ccccbc8
Subrata7a <subrata@appscode.com>
2024-05-15 11:08:42 +0600

    BasicServer

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func user(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "USER\n")
+}
+
+func main() {
+
+	http.HandleFunc("/user", user)
+	http.ListenAndServe(":8080", nil)
+}
