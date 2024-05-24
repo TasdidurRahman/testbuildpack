6060c8c9be110982a73ce082d11f3371984f4274
TasdidurRahman <tasdid@appscode.com>
2024-05-24 11:23:34 +0600

    test auto

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func user(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "USER Updated test buildpack with push update\n")
+	fmt.Fprintf(w, "USER Updated test buildpack with push update agian\n")
 }
 
 func main() {
